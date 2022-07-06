91fda743abf5fa46f90c0113cae7c7b465e98d55
MajoA <majoaguero20@hotmail.comgit config --global user.name MajoAgit commit -m 'primer commit del proyecto de la panaderia''git config --global user.email majoaguero20@hotmail.com>
2022-07-06 18:36:56 -0300

    ejercicio numero 10 resuelto

--- js/10-ejercicio.js
+++ js/10-ejercicio.js
@@ -1,1 +1,8 @@
 //Escribe un programa que pida un número y nos diga si es divisible por 2, 3, 5 o 7 (sólo hay que comprobar si lo es por uno de los cuatro)//
+
+let numero1 = parseInt(prompt('Ingrese un numero'));
+let number = (2,3,5,7)
+
+if(numero1 %(2,3,5,7) ==0){
+    document.write ('Si es divisible')
+}
