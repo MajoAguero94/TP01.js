a61f6dff6e7b020b7b6b7e6729d69ebe2e390951
MajoA <majoaguero20@hotmail.comgit config --global user.name MajoAgit commit -m 'primer commit del proyecto de la panaderia''git config --global user.email majoaguero20@hotmail.com>
2022-07-06 18:32:37 -0300

    ejercicio numero 8 resuelto

--- js/8-ejercico.js
+++ js/8-ejercico.js
@@ -1,1 +1,9 @@
 //Escribe un programa que pida un número y diga si es divisible por 2//
+let numero1 = parseInt(prompt('Ingrese un numero'));
+let numero2 = parseInt(prompt('2'));
+
+if( (numero1 % numero2) == 0){
+    document.write('Si es divisible en 2')}else{
+        document.write('No es divisible en 2')
+    }
+
