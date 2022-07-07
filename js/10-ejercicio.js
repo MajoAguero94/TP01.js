fae45798411ef24fbc150514b983b6ac6e6334eb
MajoA <majoaguero20@hotmail.comgit config --global user.name MajoAgit commit -m 'primer commit del proyecto de la panaderia''git config --global user.email majoaguero20@hotmail.com>
2022-07-06 21:11:05 -0300

    correccion ejercicio 10 y ejercicio 11 resuelto

--- js/10-ejercicio.js
+++ js/10-ejercicio.js
@@ -3,6 +3,12 @@
 let numero1 = parseInt(prompt('Ingrese un numero'));
 let number = (2,3,5,7)
 
-if(numero1 %(2,3,5,7) ==0){
-    document.write ('Si es divisible')
+if((numero1 % 2) == 0){
+    document.write ('Es divisible en : 2')
+}else if((numero1 % 3) == 0){
+    document.write ('Es divisible en : 3')
+}else if((numero1 %5 ) == 0){
+    document.write ('Es divisible en : 5')
+}else if((numero1 % 7) == 0){
+    document.write ('Es divisible en : 7')
 }
--- js/11-ejercicio.js
+++ js/11-ejercicio.js
@@ -1,1 +1,37 @@
 //Añadir al ejercicio anterior que nos diga por cuál de los cuatro es divisible (hay que decir todos por los que es divisible)//
+let numero1 = parseInt(prompt("Ingrese un numero"));
+let number = (2, 3, 5, 7);
+
+if ((numero1 % 2 == 0) & (numero1 % 3 == 0)) {
+  document.write("Es divisible en 2 y 3");
+}else if ((numero1 % 2 == 0) & (numero1 % 5 == 0)) {
+  document.write("Es divisible en 2 y 5");
+}else if ((numero1 % 2 == 0) & (numero1 % 7 == 0)) {
+  document.write("Es divisible en 2 y 7");
+}else if ((numero1 % 3 == 0) & (numero1 % 2 == 0)) {
+  document.write("Es divisible en 3 y 2");
+}else if ((numero1 % 3 == 0) & (numero1 % 5 == 0)) {
+  document.write("Es divisible en 3 y 5");
+}else if ((numero1 % 3 == 0) & (numero1 % 7 == 0)) {
+  document.write("Es divisible en 3 y 7");
+}else if ((numero1 % 5 == 0) & (numero1 % 2 == 0)) {
+  document.write("Es divisible en 5 y 2");
+}else if ((numero1 % 5 == 0) & (numero1 % 3 == 0)) {
+  document.write("Es divisible en 5 y 3");
+}else if ((numero1 % 5 == 0) & (numero1 % 7 == 0)) {
+  document.write("Es divisible en 5 y 7");
+}else if ((numero1 % 7 == 0) & (numero1 % 2 == 0)) {
+  document.write("Es divisible en 7 y 2");
+}else if ((numero1 % 7 == 0) & (numero1 % 3 == 0)) {
+  document.write("Es divisible en 7 y 3");
+}else if ((numero1 % 7 == 0) & (numero1 % 5 == 0)) {
+  document.write("Es divisible en 7 y 5");
+} else if (numero1 % 2 == 0) {
+  document.write("Es divisible en 2");
+} else if (numero1 % 3 == 0) {
+  document.write("Es divisible en 3");
+} else if (numero1 % 5 == 0) {
+  document.write("Es divisible en 5");
+} else  {
+  document.write("Es divisible en 7");
+}
